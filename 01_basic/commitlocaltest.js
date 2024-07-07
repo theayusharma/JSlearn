9ce3a8eaefb54b332d00c1bdb03867398afa8781
protocolayush <protocolayush@gmail.com>
2024-07-07 07:41:34 +0530

    typeof, function

--- 01_basic/commitlocaltest.js
+++ 01_basic/commitlocaltest.js
@@ -1,1 +1,10 @@
-console.log("this commit is from vs code in local pc")gi
+const myFunction = function(){
+    console.log("hello word");
+}
+myFunction();
+let obj={
+    name:"ayush",age:23
+}
+console.log(typeof myFunction)
+console.log(typeof obj)
+console.log(typeof obj.name)
